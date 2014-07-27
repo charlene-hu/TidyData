80d9b7d25eadecbbedc89e24fe61ca8bd0cdc7dd
Charlene Hu <charlene_hu@hotmail.com>
2014-07-27 08:36:31 -0700

    updates

--- README.md
+++ README.md
@@ -1,2 +1,69 @@
-TidyData
-========
+### Overview
+
+The **run_analysis.R** script creates a tidy data set **tidydata.txt** using data collected from the accelerometers from the Samsung Galaxy S smartphone [https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip ](https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip ).  A full description is available at the site where the data was obtained: [http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones](http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones)
+
+The run_analysis.R script performs the following tasks:
+
+1. Load raw data files
+2. Combine datasets and reshape the resulting dataset.
+3. Label activity and feature with descriptive names.
+4. Extract data of mean and standard deviation features and reshape the resulting dataset.
+5. Export the extracted dataset as tidydata.txt.
+
+
+### How to run the script
+
+Before running run_analysis.R, install below packages if you you do not have them installed already.
+
+```S
+install.packages("reshape")
+
+install.packages("stringr")
+```
+
+To generate tidydata.txt using run_analysis script, perform the following steps:
+
+1. Use source() to load run_analysis.R script. You must provide a correct path to the script. Eg. source('~/TidyData/run_analysis.R')
+
+2. Enter working directory when prompted and hit enter. Working directory should be the directory that contains test directory, train directory, features.txt, activity_labels.txt, etc.
+
+3. If the script runs successfully, you should see below messages in the console. And tidydata.txt file should be created in the working directory.
+
+```s
+setting directory...
+installing library...
+loading files...
+combining training subjects, labels and measurements...
+combining test subjects, labels and measurements...
+combining training and test data...
+melting dataset from wide format to narrow format..
+merging activity_labels dataset with tidy dataset...
+merging tidy dataset with features dataset...
+extract data of mean and standard deviation features...
+creating a wide form of the dataset...
+writing the result dataset to a text file...
+result dataset tidydata.txt created
+```
+
+### How the scripts work
+
+1. Load raw data files using read.table(). Files loaded are:
+  a. activity_labels.txt
+  b. features.txt
+  c. /test/subject_test.txt
+  d. /test/X_test.txt 
+  e. /test/y_test.txt
+  f. /train/subject_train.txt
+  g. /train/X_train.txt
+  h. /train/y_train.txt
+2. Combine training subjects, labels and measurements into one dataset using cbind.
+3. Combine test subjects, labels and measurements into one dataset using cbind
+4. Combine the training dataset and test dataset created in step 2 and step 3 using rbind.
+5. Melt the dataset created in step 4 from wide format to narrow format preparing for labeling activity and feature names in step 6.
+6. Merge activity_labels and features datasets with tidy dataset to label activity and feature with descriptive names.
+7. Extract data of mean and standard deviation features using grepl then cast the dataset from narrow format to wide format.
+8. Export the dataset created in step 7 as tidydata.txt.
+
+### Output
+
+The final output is as tidydata.txt created in the working directory. It contains subject, activity, and features mean of each variable which are of mean and standard deviation features, for each activity and each subject. 
--- run_analysis.R
+++ run_analysis.R
@@ -1,35 +1,35 @@
 ## get user input to set working directory
 wd <- readline(prompt="Please enter working directory:")
 setwd(wd)
-writeLines("seting directory")
+writeLines("setting directory...")
 directory <- getwd()
 
 ## install library
-writeLines("installing library")
+writeLines("installing library...")
 library(reshape)
 library(stringr)
 
 ## load files
-writeLines("loading files")
-activity_labels <- read.table(paste0(directory,"/UCI HAR Dataset/activity_labels.txt"))
-features <- read.table(paste0(directory,"/UCI HAR Dataset/features.txt"))
-subject_test <- read.table(paste0(directory,"/UCI HAR Dataset/test/subject_test.txt"))
-X_test <- read.table(paste0(directory,"/UCI HAR Dataset/test/X_test.txt"))
-y_test <- read.table(paste0(directory,"/UCI HAR Dataset/test/y_test.txt"))
-subject_train <- read.table(paste0(directory,"/UCI HAR Dataset/train/subject_train.txt"))
-X_train <- read.table(paste0(directory,"/UCI HAR Dataset/train/X_train.txt"))
-y_train <- read.table(paste0(directory,"/UCI HAR Dataset/train/y_train.txt"))
+writeLines("loading files...")
+activity_labels <- read.table(paste0(directory,"/activity_labels.txt"))
+features <- read.table(paste0(directory,"/features.txt"))
+subject_test <- read.table(paste0(directory,"/test/subject_test.txt"))
+X_test <- read.table(paste0(directory,"/test/X_test.txt"))
+y_test <- read.table(paste0(directory,"/test/y_test.txt"))
+subject_train <- read.table(paste0(directory,"/train/subject_train.txt"))
+X_train <- read.table(paste0(directory,"/train/X_train.txt"))
+y_train <- read.table(paste0(directory,"/train/y_train.txt"))
 
 ##combine training subjects, labels and measurements
-writeLines("combining training subjects, labels and measurements")
+writeLines("combining training subjects, labels and measurements...")
 train <- cbind(subject_train, y_train, X_train)
 
 ##combine test subjects, labels and measurements
-writeLines("combining test subjects, labels and measurements")
+writeLines("combining test subjects, labels and measurements...")
 test <- cbind(subject_test, y_test, X_test)
 
 ##combine training and test data
-writeLines("combining training and test data")
+writeLines("combining training and test data...")
 total <- rbind(train, test)
 
 ##change column names
@@ -39,38 +39,38 @@ colnames(total)[2] <- "activity"
 ##rename features by removing V so we can match by feature ID
 names(total) <- str_replace(names(total), "V", "")
 
-##melt data set from wide data to narrow data
-writeLines("melting change data set from wide data to narrow data")
+##melt dataset from wide format to narrow format
+writeLines("melting dataset from wide format to narrow format..")
 tidy <- melt(total, id=c("activity", "subject"))
 
 ##rename activity label column name
 colnames(activity_labels)[1] <- "activity"
 
-##merge activity_labels data sets with tidy data set
-writeLines("merging activity_labels data sets with tidy data set")
+##merge activity_labels  with tidy dataset
+writeLines("merging activity_labels dataset with tidy dataset...")
 tidy <- merge(activity_labels, tidy, x.by="activity", y.by="activity")[,-c(1)]
 
 ##change column names
 colnames(tidy)[1] <- "activity"
 colnames(features) <- c("variable", "feature")
 
-##merge tidy data set with features data set
-writeLines("merging tidy data set with features data set")
+##merge tidy dataset with features dataset
+writeLines("merging tidy dataset with features dataset...")
 tidy <- merge(features, tidy, x.by="variable", y.by="variable")[,-c(1)]
 
-##create a data set with only mean and std feature
-writeLines("creating a data set with only mean and std feature")
+##extract data of mean and standard deviation features
+writeLines("extract data of mean and standard deviation features...")
 pattern <- "mean\\(\\)|std\\(\\)"
 meanstd <- tidy3[grepl(pattern, tidy$feature),]
 tidy <- meanstd[c("subject", "activity", "feature", "value")]
 
-writeLines("creating a wide form of the data set")
+writeLines("creating a wide form of the dataset...")
 result <- cast(tidy, subject+ activity ~ feature, mean, value='value')
 
-##write the result data set to a text file
-writeLines("writing the result data set to a text file") 
+##write the result dataset to a text file
+writeLines("writing the result dataset to a text file...") 
 write.table(result, "tidydata.txt", sep="\t") 
 
-writeLines("result data set tidydata.txt created") 
+writeLines("result dataset tidydata.txt created") 
 
 
